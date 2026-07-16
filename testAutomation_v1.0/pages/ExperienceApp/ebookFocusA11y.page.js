04d8b8b3e9524be1422a0fe75542e2751e3056d3
Ashish <ashish725494@gmail.com>
2026-07-16 11:04:50 +0530

    Renamed the page,test , testExecution Files name

---
+++ testAutomation_v1.0/pages/ExperienceApp/ebookFocusA11y.page.js
@@ -0,0 +1,82 @@
+"use strict";
+
+// [2026-07-15] Core action library used for all DOM interactions.
+var action = require("../../core/actionLibrary/baseActionLibrary.js");
+var selectorFile = jsonParserUtil.jsonParser(selectorDir);
+
+module.exports = {
+  // Focus targets on pages
+  noteIconOnPage: selectorFile.css.ComproC1.eBook.noteIconOnPage,
+  hotlinkIconOnPage: selectorFile.css.ComproC1.eBook.hotlinkIconOnPage,
+  notesCloseBtn: selectorFile.css.ComproC1.notes.notesCloseBtn,
+  eBookNotesHeadingTxt: selectorFile.css.ComproC1.notes.eBookNotesHeadingTxt,
+  homeButton: selectorFile.css.ComproC1.eBook.homeButton,
+  pageNOShow: selectorFile.css.ComproC1.pageNoDialogBox.pageNOShow,
+  readerOuterContainer: selectorFile.css.ComproC1.eBook.readerOuterContainer,
+
+
+  isInitialized: async function () {
+    var res;
+    await logger.logInto(await stackTrace.get());
+    await action.waitForDocumentLoad();
+    res = {
+      pageStatus: await action.waitForDisplayed(this.homeButton, 60000),
+    };
+    return res;
+  },
+
+  resetFocusToReader: async function () {
+    await logger.logInto(await stackTrace.get());
+    return await action.focus(this.readerOuterContainer);
+  },
+
+  pressTab: async function () {
+    await logger.logInto(await stackTrace.get());
+    return await action.pressTab();
+  },
+
+  pressShiftTab: async function () {
+    await logger.logInto(await stackTrace.get());
+    return await action.pressShiftTab();
+  },
+
+  pressEnter: async function (selectorName) {
+    await logger.logInto(await stackTrace.get(), "press Enter on selector: " + selectorName);
+    const sel = this[selectorName];
+    return await action.pressEnter(sel);
+  },
+
+  assertFocusOn: async function (selectorName, message) {
+    await logger.logInto(await stackTrace.get(), "assert focus on: " + selectorName);
+    const sel = this[selectorName];
+    return await action.assertFocusOn(sel, message);
+  },
+
+  assertPanelVisible: async function (selectorName, message) {
+    await logger.logInto(await stackTrace.get(), "assert panel visible: " + selectorName);
+    const sel = this[selectorName];
+    return await action.assertPanelVisible(sel, message);
+  },
+
+  assertPanelClosed: async function (selectorName, message) {
+    await logger.logInto(await stackTrace.get(), "assert panel closed: " + selectorName);
+    const sel = this[selectorName];
+    return await action.assertPanelClosed(sel, message);
+  },
+
+  assertOnPage: async function (expectedPage, message) {
+    await logger.logInto(await stackTrace.get(), "assert current page is: " + expectedPage);
+    return await action.assertOnPage(this.pageNOShow, expectedPage, message);
+  },
+
+  waitForNoteOnPage: async function () {
+    await logger.logInto(await stackTrace.get());
+    return await action.waitForDisplayed(this.noteIconOnPage, 10000);
+  },
+
+  waitForHotlinkOnPage: async function () {
+    await logger.logInto(await stackTrace.get());
+    return await action.waitForDisplayed(this.hotlinkIconOnPage, 10000);
+  },
+
+};
---
+++ testAutomation_v1.0/test/ExperienceApp/ebookFocusA11y.test.js
@@ -0,0 +1,169 @@
+"use strict";
+
+// [2026-07-15] Page object used for eBook keyboard focus accessibility assertions and navigation.
+var focusPage = require("../../pages/ExperienceApp/ebookFocusA11y.page.js");
+var action = require("../../core/actionLibrary/baseActionLibrary.js");
+var sts;
+
+module.exports = {
+  // --- Suite 1: Page 22 (Notes only) ---
+
+  TST_KBOA_TC_1: async function (testdata) {
+    // 1. Go to page 22 and wait for Notes icon to be rendered
+    sts = await action.goToPage(22);
+    await assertion.assertEqual(sts, true, "eBook failed to navigate to page 22");
+    sts = await focusPage.waitForNoteOnPage();
+    await assertion.assertEqual(sts, true, "Note icon on page 22 was not displayed");
+    sts = await focusPage.resetFocusToReader();
+    await assertion.assertEqual(sts, true, "Failed to reset focus to reader");
+  },
+
+  TST_KBOA_TC_2: async function (testdata) {
+    // 2. Press Tab to focus on the Notes element
+    sts = await focusPage.pressTab();
+    await assertion.assertEqual(sts, true, "Failed to press Tab key");
+    sts = await focusPage.assertFocusOn("noteIconOnPage", "Focus did not land on the Note element on page 22");
+    await assertion.assertEqual(sts, true, "Note element focus assertion failed");
+  },
+
+  TST_KBOA_TC_3: async function (testdata) {
+    // 3. Press Tab to focus on the Home button
+    sts = await focusPage.pressTab();
+    await assertion.assertEqual(sts, true, "Failed to press Tab key");
+    sts = await focusPage.assertFocusOn("homeButton", "Focus did not land on the Home button after Note on page 22");
+    await assertion.assertEqual(sts, true, "Home button focus assertion failed");
+  },
+
+  TST_KBOA_TC_4: async function (testdata) {
+    // 4. Press Shift+Tab to move focus back to Notes element
+    sts = await focusPage.pressShiftTab();
+    await assertion.assertEqual(sts, true, "Failed to press Shift+Tab key");
+    sts = await focusPage.assertFocusOn("noteIconOnPage", "Focus did not move back to the Note element on page 22");
+    await assertion.assertEqual(sts, true, "Note element focus shift assertion failed");
+  },
+
+  TST_KBOA_TC_5: async function (testdata) {
+    // 5. Keyboard click the Notes element to open notes panel
+    await browser.pause(1500);
+    sts = await focusPage.pressEnter("noteIconOnPage");
+    await assertion.assertEqual(sts, true, "Failed to keyboard click Note element");
+    sts = await focusPage.assertPanelVisible("eBookNotesHeadingTxt", "Notes panel was not visible after keyboard click");
+    await assertion.assertEqual(sts, true, "Notes panel visibility assertion failed");
+  },
+
+  TST_KBOA_TC_6: async function (testdata) {
+    // 6. Keyboard click the close button on the Notes panel to close it
+    await browser.pause(1500);
+    sts = await focusPage.pressEnter("notesCloseBtn");
+    await assertion.assertEqual(sts, true, "Failed to keyboard click Notes close button");
+    sts = await focusPage.assertPanelClosed("eBookNotesHeadingTxt", "Notes panel was still visible after click close");
+    await assertion.assertEqual(sts, true, "Notes panel closure assertion failed");
+  },
+
+  // --- Suite 2: Page 24 (Hotlink only) ---
+
+  TST_KBOA_TC_7: async function (testdata) {
+    // 1. Go to page 24 and wait for Hotlink icon to be rendered
+    sts = await action.goToPage(24);
+    await assertion.assertEqual(sts, true, "eBook failed to navigate to page 24");
+    sts = await focusPage.waitForHotlinkOnPage();
+    await assertion.assertEqual(sts, true, "Hotlink icon on page 24 was not displayed");
+    sts = await focusPage.resetFocusToReader();
+    await assertion.assertEqual(sts, true, "Failed to reset focus to reader");
+  },
+
+  TST_KBOA_TC_8: async function (testdata) {
+    // 2. Press Tab to focus on the Hotlink element
+    sts = await focusPage.pressTab();
+    await assertion.assertEqual(sts, true, "Failed to press Tab key");
+    sts = await focusPage.assertFocusOn("hotlinkIconOnPage", "Focus did not land on the Hotlink element on page 24");
+    await assertion.assertEqual(sts, true, "Hotlink element focus assertion failed");
+  },
+
+  TST_KBOA_TC_9: async function (testdata) {
+    // 3. Press Tab to focus on the Home button
+    sts = await focusPage.pressTab();
+    await assertion.assertEqual(sts, true, "Failed to press Tab key");
+    sts = await focusPage.assertFocusOn("homeButton", "Focus did not land on the Home button after Hotlink on page 24");
+    await assertion.assertEqual(sts, true, "Home button focus assertion failed");
+  },
+
+  TST_KBOA_TC_10: async function (testdata) {
+    // 4. Press Shift+Tab to move focus back to Hotlink element
+    sts = await focusPage.pressShiftTab();
+    await assertion.assertEqual(sts, true, "Failed to press Shift+Tab key");
+    sts = await focusPage.assertFocusOn("hotlinkIconOnPage", "Focus did not move back to the Hotlink element on page 24");
+    await assertion.assertEqual(sts, true, "Hotlink element focus shift assertion failed");
+  },
+
+  TST_KBOA_TC_11: async function (testdata) {
+    // 5. Keyboard click the Hotlink element to trigger page navigation jump
+    await browser.pause(1500);
+    sts = await focusPage.pressEnter("hotlinkIconOnPage");
+    await assertion.assertEqual(sts, true, "Failed to keyboard click Hotlink element");
+    sts = await focusPage.assertOnPage(28, "eBook did not navigate to page 28 after keyboard click");
+    await assertion.assertEqual(sts, true, "Hotlink page jump assertion failed");
+  },
+
+  TST_KBOA_TC_12: async function (testdata) {
+    // 6. Navigate back to page 24 to leave suite in clean state
+    sts = await action.goToPage(24);
+    await assertion.assertEqual(sts, true, "Failed to navigate back to page 24");
+  },
+
+  // --- Suite 3: Page 26 (Neither Notes nor Hotlink) ---
+
+  TST_KBOA_TC_13: async function (testdata) {
+    // 1. Go to page 26
+    sts = await action.goToPage(26);
+    await assertion.assertEqual(sts, true, "eBook failed to navigate to page 26");
+    sts = await focusPage.resetFocusToReader();
+    await assertion.assertEqual(sts, true, "Failed to reset focus to reader");
+  },
+
+  TST_KBOA_TC_14: async function (testdata) {
+    // 2. Press Tab to focus directly on the Home button
+    sts = await focusPage.pressTab();
+    await assertion.assertEqual(sts, true, "Failed to press Tab key");
+    sts = await focusPage.assertFocusOn("homeButton", "Focus did not land directly on the Home button on page 26");
+    await assertion.assertEqual(sts, true, "Home button focus assertion failed");
+  },
+
+  // --- Suite 4: Page 28 (Both Notes and Hotlink) ---
+
+  TST_KBOA_TC_15: async function (testdata) {
+    // 1. Go to page 28 and wait for Note and Hotlink icons
+    sts = await action.goToPage(28);
+    await assertion.assertEqual(sts, true, "eBook failed to navigate to page 28");
+    sts = await focusPage.waitForNoteOnPage();
+    await assertion.assertEqual(sts, true, "Note icon on page 28 was not displayed");
+    sts = await focusPage.waitForHotlinkOnPage();
+    await assertion.assertEqual(sts, true, "Hotlink icon on page 28 was not displayed");
+    sts = await focusPage.resetFocusToReader();
+    await assertion.assertEqual(sts, true, "Failed to reset focus to reader");
+  },
+
+  TST_KBOA_TC_16: async function (testdata) {
+    // 2. Press Tab to focus on the Note element
+    sts = await focusPage.pressTab();
+    await assertion.assertEqual(sts, true, "Failed to press Tab key");
+    sts = await focusPage.assertFocusOn("noteIconOnPage", "Focus did not land on the Note element first on page 28");
+    await assertion.assertEqual(sts, true, "Note element focus assertion failed");
+  },
+
+  TST_KBOA_TC_17: async function (testdata) {
+    // 3. Press Tab to focus on the Hotlink element
+    sts = await focusPage.pressTab();
+    await assertion.assertEqual(sts, true, "Failed to press Tab key");
+    sts = await focusPage.assertFocusOn("hotlinkIconOnPage", "Focus did not land on the Hotlink element second on page 28");
+    await assertion.assertEqual(sts, true, "Hotlink element focus assertion failed");
+  },
+
+  TST_KBOA_TC_18: async function (testdata) {
+    // 4. Press Tab to focus on the Home button
+    sts = await focusPage.pressTab();
+    await assertion.assertEqual(sts, true, "Failed to press Tab key");
+    sts = await focusPage.assertFocusOn("homeButton", "Focus did not land on the Home button third on page 28");
+    await assertion.assertEqual(sts, true, "Home button focus assertion failed");
+  }
+};
